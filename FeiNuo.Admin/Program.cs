27091c2036a90eb1d91f330e20a928e76e384fde
wangjialiang <wjlxxfn@gmail.com>
2025-02-13 15:31:21 +0800

    加入依赖包，配置项目注入依赖

--- FeiNuo.Admin/Program.cs
+++ FeiNuo.Admin/Program.cs
@@ -1,8 +1,11 @@
-﻿global using FeiNuo.Core;
+﻿global using FeiNuo.AspNetCore;
+global using FeiNuo.Core;
 using FeiNuo.Admin.Models;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Events;
+using System.Reflection;
 
 namespace FeiNuo.Admin;
 
@@ -37,31 +40,49 @@ public class Program
                 }
             });
 
-            builder.Services.AddControllers();
+            // 自定义的缓存需要在AddAppServices前注入
+            // builder.Services.AddDistributedMemoryCache();
+
+            // 注入各项服务
+            builder.Services.AddAppServices();
+            builder.Services.AddAppControllers();
+            // 注入认证授权
+            builder.Services.AddAppSecurity(builder.Configuration);
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Mapster，扫描IRegister接口的类，自动配置
+            TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            // 接口文档
+            app.MapOpenApi();
+            app.UseSwaggerUI(options =>
             {
-                app.MapOpenApi();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/openapi/v1.json", "v1");
-                });
+                options.SwaggerEndpoint("/openapi/v1.json", "v1");
+            });
 
-                DbInitializer.EnsureDatabaseCreated(app.Services, false);
-            }
 
-            app.UseHttpsRedirection();
 
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
+            // 跨域
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            // 认证授权
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
+            // 控制器
             app.MapControllers();
 
+            // 开发环境
+            //if (app.Environment.IsDevelopment())
+            //{
+            // 确保数据库已经创建
+            DbInitializer.EnsureDatabaseCreated(app.Services, false);
+            //}
             app.Run();
         }
         catch (Exception ex)
