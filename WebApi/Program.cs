52c2e76a33b4db45b77383237b04879c20213394
wangjialiang <wjlxxfn@gmail.com>
2025-02-13 13:54:23 +0800

    添加 SwaggerUI

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -19,6 +19,10 @@ namespace WebApi
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection();
