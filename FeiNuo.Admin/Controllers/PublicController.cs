4964cb98f59f9e97170a760a3667ea9499ab468c
wangjialiang <wjlxxfn@gmail.com>
2025-02-23 11:34:15 +0800

    添加一个公共的控制器

---
+++ FeiNuo.Admin/Controllers/PublicController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeiNuo.Admin.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class PublicController : ControllerBase
+    {
+        private readonly ILogger<PublicController> logger;
+
+        public PublicController(ILogger<PublicController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public string Test()
+        {
+            logger.LogDebug("debug,{a}", DateTime.Now);
+            logger.LogInformation("information,{now}", DateTime.Now);
+            logger.LogWarning("warning,{now}", DateTime.Now);
+            logger.LogError("Error,{now}", DateTime.Now);
+
+            return "success";
+        }
+    }
+}
